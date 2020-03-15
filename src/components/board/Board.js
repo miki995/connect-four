42d22342bab0e2e3f1a123f7d0aa90ea93d3e072
Miroslav Maksimovic <miroslavmaksimovic95@gmail.com>
2020-03-15 11:14:50 +0100

    Changes:
    - Setup Board logic for Column component and state of the board

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import React, { Component } from "react";
 // css
 import './Board.css'
 
@@ -7,28 +7,35 @@ export default class Board extends Component {
 
     rowsNumber = 7;
     columnsNumber = 6;
-
+    columnsArray = new Array(this.columnsNumber).fill(null);
+    initialBoardState = new Array(this.rowsNumber).fill(this.columnsArray);
 
     state = {
+        boardState: this.initialBoardState,
         player: 'Red',
         winner: ''
     };
 
     render() {
 
+        const winnerColor = (this.state.winner === 'Red') ? 'Red' : 'Yellow';
+        const color = !this.state.winner ? winnerColor : null;
+
+        const columns = [...Array(this.state.boardState.length)].map((x, index) =>
+            <Column
+                key={index}
+                fields={this.state.boardState[index]}
+                color={color}
+                handleClick={() => this.next(index)}
+            />
+        );
+
 
         return (
             <div>
                 {this.state.winner && <div className='Winner'>The winner is player: {this.state.winner}</div>}
-                <div className='Board'>
-                    <div>
-                        Column
-                    <div>field</div>
-                    </div>
-                </div>
+                <div className='Board'>{columns}</div>
             </div>
         )
     }
-
-
 }
