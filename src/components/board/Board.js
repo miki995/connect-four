bbfc65e07955eb3771f3a57c3fae3d76b74c6fe0
Miroslav Maksimovic <miroslavmaksimovic95@gmail.com>
2020-03-15 11:18:12 +0100

    Changes:
    - Update the state of the board when user press on column

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -16,6 +16,30 @@ export default class Board extends Component {
         winner: ''
     };
 
+    next(columnId) {
+
+        if (this.state.winner !== '') {
+            return;
+        }
+
+        const boardState = this.state.boardState.map(arr => arr.slice());
+
+        if (boardState[columnId].indexOf(null) === -1) {
+            return;
+        }
+
+        let newColumn = boardState[columnId].reverse();
+        newColumn[newColumn.indexOf(null)] = this.state.player;
+        newColumn.reverse();
+
+        const player = (this.state.player === 'Red') ? 'Yellow' : 'Red';
+
+        this.setState({
+            player,
+            boardState
+        })
+    }
+
     render() {
 
         const winnerColor = (this.state.winner === 'Red') ? 'Red' : 'Yellow';
