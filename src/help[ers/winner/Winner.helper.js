e3c9119efa0c03655c61cb6a117fa198e554a57f
Miroslav Maksimovic <miroslavmaksimovic95@gmail.com>
2020-03-15 11:29:46 +0100

    Changes:
    - Check rows complete

--- src/help[ers/winner/Winner.helper.js
+++ src/help[ers/winner/Winner.helper.js
@@ -3,10 +3,13 @@ export function checkIsWinn(board) {
     // check rows
     for (let row = 0; row < 6; row++)
         for (let column = 0; column < 4; column++)
-            if (checkLine())
+            if (checkLine(
+                board[column][row],
+                board[column + 1][row],
+                board[column + 2][row],
+                board[column + 3][row])
+            )
                 return board[column][row];
-
-    return '';
 }
 
 
